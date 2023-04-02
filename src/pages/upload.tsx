5db99bfa0d74f17fa3a0ab25b1c2675871e2dee2
krarpit <kumar.arpit77@yahoo.com>
2023-04-02 16:34:00 -0700

    required asterix

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -93,7 +93,7 @@ export default function Upload() {
               <form action="#">
                   <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
                       <div className="sm:col-span-2">
-                          <label htmlFor="residence" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Residence</label>
+                          <label htmlFor="residence" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Residence*</label>
                           <select id="residence" defaultValue="SR" onChange={ e => setResidence(e.target.selectedOptions[0].innerHTML)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                               <option disabled value="SR">Select residence</option>
                               <option value="WG">Walter Gage</option>
